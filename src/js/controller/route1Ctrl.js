f08e587263eb10ef979c5f21178c38a81a336916
朱庆松 <zqs@zqs.local>
2017-09-07 16:37:39 +0800

    no message

--- src/js/controller/route1Ctrl.js
+++ src/js/controller/route1Ctrl.js
@@ -2,7 +2,7 @@ module.exports=[
 	'$scope','$state',
 	function($scope,$state){
 		console.log('route1');
-		$scope.ttt = false;
+		$scope.ttt = true;
 		$scope.goRoute2=function(){
 			$scope.ttt = !$scope.ttt;
 			// $state.go('route2');
